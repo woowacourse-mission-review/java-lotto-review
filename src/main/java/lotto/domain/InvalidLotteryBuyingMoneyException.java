91d2e5c1cb969c3f94452628fb628e3af9061285
green4469 <green4469@gmail.com>
2019-12-08 22:02:55 +0900

    feat: lottery buying money should be multiple of lottery price.

---
+++ src/main/java/lotto/domain/InvalidLotteryBuyingMoneyException.java
@@ -0,0 +1,9 @@
+package lotto.domain;
+
+public class InvalidLotteryBuyingMoneyException extends RuntimeException {
+    private static final String ERROR_MSG = "로또 구입 금액은 로또 가격에 자연수를 곱한 값이어야 합니다.";
+
+    public InvalidLotteryBuyingMoneyException() {
+        super(ERROR_MSG);
+    }
+}
---
+++ src/main/java/lotto/domain/LotteryBuyingMoney.java
@@ -0,0 +1,29 @@
+package lotto.domain;
+
+public class LotteryBuyingMoney {
+    private static final long LOTTERY_PRICE = 1000L;
+    private final long amount;
+
+    private LotteryBuyingMoney(long amount) {
+        validateAmount(amount);
+        this.amount = amount;
+    }
+
+    private void validateAmount(long amount) {
+        if (isNotDivisibleByLotteryPrice(amount) || isLowerThanOrEqualToZero(amount)) {
+            throw new InvalidLotteryBuyingMoneyException();
+        }
+    }
+
+    private boolean isNotDivisibleByLotteryPrice(long amount) {
+        return amount % LOTTERY_PRICE != 0;
+    }
+
+    private boolean isLowerThanOrEqualToZero(long amount) {
+        return amount <= 0;
+    }
+
+    public static LotteryBuyingMoney from(long amount) {
+        return new LotteryBuyingMoney(amount);
+    }
+}
