e26591a1cc3f4960820a7b0861c9142f4c24e668
green4469 <green4469@gmail.com>
2019-12-08 22:13:46 +0900

    feat: cache lottery numbers

--- src/main/java/lotto/domain/LotteryNumber.java
+++ src/main/java/lotto/domain/LotteryNumber.java
@@ -1,22 +1,32 @@
 package lotto.domain;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LotteryNumber {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 45;
+    private static final Map<Integer, LotteryNumber> CACHE = new HashMap<>();
     private final int number;
 
-    private LotteryNumber(int lotteryNumber) {
-        validateLotteryNumber(lotteryNumber);
-        this.number = lotteryNumber;
-    }
-
-    private void validateLotteryNumber(int lotteryNumber) {
-        if (lotteryNumber < MIN_NUMBER || lotteryNumber > MAX_NUMBER) {
-            throw new InvalidLotteryNumberException();
+    static {
+        for (int lotteryNumber = MIN_NUMBER; lotteryNumber < MAX_NUMBER; lotteryNumber++) {
+            CACHE.put(lotteryNumber, new LotteryNumber(lotteryNumber));
         }
     }
 
+    private LotteryNumber(int lotteryNumber) {
+        this.number = lotteryNumber;
+    }
+
     public static LotteryNumber from(int lotteryNumber) {
-       return new LotteryNumber(lotteryNumber);
+        validateLotteryNumber(lotteryNumber);
+       return CACHE.get(lotteryNumber);
+    }
+
+    private static void validateLotteryNumber(int lotteryNumber) {
+        if (lotteryNumber < MIN_NUMBER || lotteryNumber > MAX_NUMBER) {
+            throw new InvalidLotteryNumberException();
+        }
     }
 }
