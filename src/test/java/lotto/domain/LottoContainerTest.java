9af48f98aeb50cac111deaabd5a15bbf604de9f4
skygl <jupiter9501@gmail.com>
2019-12-08 17:49:49 +0900

    test: LottoContainer 테스트 추가

--- src/test/java/lotto/domain/LottoContainerTest.java
+++ src/test/java/lotto/domain/LottoContainerTest.java
@@ -5,12 +5,39 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
 import java.util.Arrays;
+import java.util.List;
 
 import static lotto.domain.Rank.*;
 import static lotto.utils.LottoUtils.parse;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class LottoContainerTest {
+
+    private static final List<Lotto> LOTTOS = Arrays.asList(
+            Lotto.of(parse(1, 2, 3, 4, 5, 6)),
+            Lotto.of(parse(38, 39, 40, 4, 5, 6)),
+            Lotto.of(parse(24, 26, 28, 4, 5, 6)));
+
+    @DisplayName("LottoContainer 객체 생성")
+    @Test
+    void create_container() {
+        assertDoesNotThrow(() -> new LottoContainer(LOTTOS));
+    }
+
+    @DisplayName("다른 LottoContainer 로부터 LottoContainer 객체 생성")
+    @Test
+    void create_container_from_other_container() {
+        LottoContainer lottoContainer = new LottoContainer(LOTTOS);
+        assertDoesNotThrow(() -> new LottoContainer(lottoContainer, LOTTOS));
+    }
+
+    @DisplayName("getLottos 로 꺼낸 로또 리스트가 생성될 때 입력받은 리스트와 같은지")
+    @Test
+    void assert_getter() {
+        LottoContainer lottoContainer = new LottoContainer(LOTTOS);
+        assertEquals(LOTTOS, lottoContainer.getLottos());
+    }
 
     @DisplayName("결과 생성 확인")
     @Test
